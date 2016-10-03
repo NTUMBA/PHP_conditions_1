47249259161dc0eb988ab4a88ecaf1d871f6832d
NTUMBA <cityzenmboma@yahoo.fr>
2016-10-03 15:47:48 +0200

    fixe bracket position and file name. Add function for integer

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html>
   <head>
-   <title>Renseignements</title>
+   <title>Object form 1</title>
    <link type="text/css" rel="stylesheet" href="style.css">
    <meta charset="utf-8"/>
  </head>
  <body>
 
- <form action="cible.php" method="post">
+ <form action="object.php" method="post" enctype="multipart/form-data">
    <!-- Grâce à enctype, le navigateur du visiteur sait qu'il s'apprête à envoyer des fichiers. -->
    <p>
              Formulaire d'envoi de fichier :<br />
@@ -31,7 +31,7 @@
        <!--checkbox allows to select more then 1 option, for instance user can choose the first and the last one  -->
 
 <!-- le code ci-dessous donne la passibilité au développeur de cibler le fichier formulaire situé dans osn ordinateur  -->
-<!-- <input type="file" name="index" /><br /> -->
+<input type="file" name="renseignement" /><br />
 <!-- ce code permet d'envoyer le fichier -->
 <input type="submit"  name="submit" value="Envoyer le fichier" />
 </form>
--- object.php
+++ object.php
@@ -1,38 +1,68 @@
-<?php
-if (isset($_FILES['renseignement']) AND $_FILES['renseignement']['error'] == 0 )
-{
-if (isset($_POST['nom']) AND isset($_POST['prenom'])){
-echo 'Bonjour'. ' ' .htmlspecialchars($_POST['nom']).' ' .htmlspecialchars($_POST['prenom']);
-}
-else {
-echo 'Vous avez oublié d\'indiquez votre nom et/ou prénom';
-}
-if (isset($_POST['age']) AND (int)$_POST['age']){
-echo 'Vous avez'. ' ' .htmlspecialchars($_POST['age']);
-}
-else{
-echo 'Veuillez écrire votre âge en nombre entier';
-}
-if (isset($_POST['sexe'])){
-echo 'Vous êtes un '. ' '.htmlspecialchars($_POST['sexe']);
-}
-else {
-echo 'Vous n\'êtes ni un homme ni une femme...un problème?';
-}
-if (isset($_POST['ville'])){
-echo 'Vous venez de la ville de'. ' ' .htmlspecialchars($_POST['ville']);
-}
-else{
-echo 'D\'où venez-vous?';
-}
-if (isset($_POST['loisirs'])){
-foreach ($_POST['loisirs'] as $simon=>$value) {
-  echo 'vous aimez'. ' ' .htmlspecialchars($value). '</br>';
-}
+<!DOCTYPE html>
+ <html>
+   <head>
+    <title>Object form 1</title>
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <meta charset="utf-8"/>
+  </head>
+  <body>
 
- }
- else
-{  echo 'No file found or error in your file';
-}
-?>
-<p>N'hésitez pas à retourner sur la fiche de renseignement afin d'apportez <a href="renseignement.php">des rectification en cliquant ici</a></p>
+    <?php
+      if (isset($_FILES['renseignement']) AND $_FILES['renseignement']['error'] == 0 ){
+          if ($_FILES['renseignement']['size'] <= 2000000){
+             $infosfichier = pathinfo($_FILES['renseignement']['name']);
+             $extension_upload = $infosfichier['extension'];
+             $extensions_autorisees = array('html', 'php');
+             if (in_array($extension_upload, $extensions_autorisees)){
+               move_uploaded_file($_FILES['renseignement']['tmp_name'], 'uploads/' . basename($_FILES['renseignement']['name']));
+                              echo 'L\'envoi a bien été effectué !'. '</br>';
+             }
+          }
+          if (isset($_POST['nom']) AND isset($_POST['prenom'])){
+              echo 'Bonjour'. ' ' .htmlspecialchars($_POST['nom']).' ' .htmlspecialchars($_POST['prenom']). '</br>';
+          }
+          else {
+              echo 'Vous avez oublié d\'indiquez votre nom et/ou prénom';
+          }
+          if (isset($_POST['age']) AND $_POST['age'] == is_int){
+              echo 'Vous avez'. ' ' .htmlspecialchars($_POST['age']). '</br>';
+          }
+          else{
+              echo 'Veuillez écrire votre âge en nombre entier'. '</br>';
+          }
+          if (isset($_POST['sexe'])){
+            if ($_POST['sexe']== 'homme'){
+              echo 'Vous êtes un '. ' '.htmlspecialchars($_POST['sexe']). '</br>';
+            }
+            else{
+              echo 'Vous êtes une '. ' '.htmlspecialchars($_POST['sexe']). '</br>';
+          }
+        }
+          else {
+              echo 'Vous n\'êtes ni un homme ni une femme...un problème?'. '</br>';
+          }
+          if (isset($_POST['ville'])){
+              echo 'Vous venez de la ville de'. ' ' .htmlspecialchars($_POST['ville']). '</br>';
+          }
+          else{
+              echo 'D\'où venez-vous?';
+          }
+          ?>
+          <h5>Loisirs:</h5>
+          <?php
+          if (isset($_POST['loisirs'])){
+            foreach ($_POST['loisirs'] as $simon=>$value) {
+              echo htmlspecialchars($value). '</br>';
+          }
+
+       }
+       else{
+           echo 'No file found or error in your file';
+      }
+    }
+      ?>
+      <h5>Remarque:</h5>
+    <p>N'hésitez pas à retourner sur la fiche de renseignement afin d'apportez <a href="index.php">des rectifications en cliquant ici</a></p>
+
+  </body>
+</html>
